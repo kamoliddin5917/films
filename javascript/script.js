09f3c14f6df18dc06e467ad9b6b35f9ca6747cc2
toxir-brat <toxirli94@gmail.com>
2021-07-11 13:49:15 +0500

    modal oyna yasaldi

--- javascript/script.js
+++ javascript/script.js
@@ -6,6 +6,13 @@ var select = findEl(".select");
 var form = findEl(".form");
 var btnForm = findEl(".btn--form");
 var searchInput = findEl(".search__input");
+var selectSort = findEl(".js-select-sort");
+
+var myModal = findEl(".js-modal");
+var myModalBtn = findEl(".js-modal__btn");
+var myModalH1 = findEl(".js-modal__title");
+var myModalP = findEl(".js-modal__text");
+var myModalOl = findEl(".js-modal__ol");
 
 var creatEl = (element) => document.createElement(element); // elementlani yaratadi
 
@@ -17,7 +24,7 @@ var usul_3 = (kino) => {
   var newBtnText = creatEl("button");
   var newText = creatEl("p");
   var newBtnCard = creatEl("div");
-  var newBtn = creatEl("a");
+  var newBtn = creatEl("button");
   var newLikes = creatEl("div");
   var newBtnLike = creatEl("button");
   var newBtnNotLike = creatEl("button");
@@ -54,10 +61,19 @@ var usul_3 = (kino) => {
   // newGenresUl.className = "card__genres-ul"; // kinolani ganriga ul yaratadi
 
   newImg.src = kino.poster;
-  newDate.textContent = kino.release_date;
+  newDate.textContent =
+    new Date(kino.release_date).getFullYear() +
+    "-year " +
+    new Date(kino.release_date).getHours() +
+    ":" +
+    new Date(kino.release_date).getMinutes() +
+    ":" +
+    new Date(kino.release_date).getSeconds() +
+    "-time";
   newTitle.textContent = kino.title;
   newText.textContent = kino.overview;
-  newBtn.textContent = "Play";
+  newBtn.textContent = "More";
+  newBtn.dataset.id = kino.id;
 
   /* // kino.genres.forEach(function (genre) {
   // //  var newGenresLi = document.createElement("li");
@@ -75,19 +91,47 @@ var usul_3 = (kino) => {
 
   // newLi.append(newGenresUl);   // kinolani ganridigi UL i kinoladigi LI ni ichiga soladi
 
-  newBtnText.addEventListener("click", function () {
+  newBtnText.addEventListener("click", () => {
     newText.classList.toggle("card__text-click");
     newBtnText.classList.toggle("card__btn-text-click");
   });
 
+  var modalOpen = (event) => {
+    var cinema = findFilm(event.target.dataset.id);
+    myModalH1.textContent = cinema.title;
+    myModalP.textContent = cinema.overview;
+
+    myModalOl.innerHTML = "";
+    cinema.genres.forEach((genre) => {
+      var newModalLi = creatEl("li");
+      newModalLi.textContent = genre;
+      newModalLi.className = "modal__list";
+      myModalOl.append(newModalLi);
+    });
+
+    myModal.classList.add("modal__open");
+  };
+  newBtn.addEventListener("click", modalOpen);
+
   cardsUl.append(newLi);
 };
+
+var modalClose = (event) => {
+  if (event.target === myModal || event.target === myModalBtn) {
+    myModal.classList.remove("modal__open");
+  }
+};
+myModal.addEventListener("click", modalClose);
+
+var findFilm = (id) => films.find((kino) => kino.id == id);
 
 function forFilms(films) {
   // forFilms function bn aylanish 2-usul
   films.forEach((kino) => usul_3(kino)); // usul_3 digan functionni chaqirib kino digan qiymat berildi
 }
 forFilms(films);
+
+/*  var forFilms = (films) => films.forEach((kino) => usul_3(kino)); forFilms(films); */ // tepadigi function ni optimali
 
 //  for(var kino of films){  // for of bn aylanish 1-usul
 // usul_3(kino); // 3-usul
@@ -142,6 +186,34 @@ forFilms(films);
   cardsUl.append(newLi); */ // 2-usul
 // }
 
+var sotrNewOld = (a, b) => b.release_date - a.release_date;
+var sotrOldNew = (a, b) => a.release_date - b.release_date;
+var sortAaZz = (a, b) => {
+  if (a.title > b.title) {
+    return 1;
+  }
+  if (a.title < b.title) {
+    return -1;
+  }
+  return 0;
+};
+var sortZzAa = (a, b) => {
+  if (a.title > b.title) {
+    return -1;
+  }
+  if (a.title < b.title) {
+    return 1;
+  }
+  return 0;
+};
+
+var sortObj = {
+  0: sotrNewOld,
+  1: sotrOldNew,
+  2: sortAaZz,
+  3: sortZzAa,
+};
+
 function getGenres(films) {
   // janrlani bittadan qilib option yaratib ichiga soladi optionni select ichiga soladi
   let genres = [];
@@ -171,7 +243,7 @@ form.addEventListener("submit", function (params) {
 
   cardsUl.innerHTML = "";
   var selectValue = select.value;
-  var searchValue = searchInput.value;
+  var searchValue = searchInput.value.trim();
   var searchValueRegExp = new RegExp(searchValue, "gi");
 
   var resaultFilter = films.filter(function (filtr) {
@@ -182,7 +254,7 @@ form.addEventListener("submit", function (params) {
     if (filtr.genres.includes(selectValue)) {
       return filtr;
     }
-  });
+  }); // janrla bo'yicha filtirlidi
 
   // var searchInputReg = resaultFilter.filter((film) =>
   //   film.title.toLowerCase().includes(searchValue.toLowerCase())
@@ -194,9 +266,11 @@ form.addEventListener("submit", function (params) {
   //   }
   // });  // inputtan qidirishi RegExp bn 1-usuli
 
-  var searchInputReg = resaultFilter.filter((cinema) =>
-    cinema.title.match(searchValueRegExp)
-  ); // inputtan qidirishi RegExp bn 2-usuli
+  var sortSelectValue = selectSort.value;
+
+  var searchInputReg = resaultFilter
+    .filter((cinema) => cinema.title.match(searchValueRegExp)) // inputtan qidirishi RegExp bn 2-usuli
+    .sort(sortObj[sortSelectValue]); // inputti filtrlab sortlaberish
 
   // resaultFilter.forEach((result) => usul_3(result)); // for of bn aylanadi 1-usul
 
